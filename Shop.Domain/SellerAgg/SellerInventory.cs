950808f8302577bbdf0a92827822ad77425b3bd4
Kamyab <87451309+kmybrostmi@users.noreply.github.com>
2022-10-10 23:00:29 +0330

    Add Seller Aggregate

---
+++ Shop.Domain/SellerAgg/SellerInventory.cs
@@ -0,0 +1,38 @@
+﻿using Common.Domain;
+using Shop.Domain.OrderAgg;
+
+namespace Shop.Domain.SellerAgg;
+
+public class SellerInventory : BaseEntity
+{
+    public SellerInventory(Guid productId, int count, int price, int? discountPercentage = null)
+    {
+        if(price < 1 || count < 0)
+        {
+            throw new InvalidDomainDataException();
+        }
+        ProductId = productId;
+        Count = count;
+        Price = price;
+        DiscountPercentage = discountPercentage;
+    }
+
+    public Guid SellerId { get; internal set; }
+    public Guid ProductId { get; private set; }
+    public int Count { get; private set; }
+    public int Price { get; private set; }
+    public int? DiscountPercentage { get; private set; }
+
+    public void Edit(int count, int price, int? discountPercentage = null)
+    {
+        if (price < 1 || count < 0)
+            throw new InvalidDomainDataException();
+
+        Count = count;
+        Price = price;
+        DiscountPercentage = discountPercentage;
+    }
+}
+
+
+
