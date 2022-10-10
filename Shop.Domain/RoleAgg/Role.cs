094e05101d0729f67fc8d50c0fa12bdf71886e14
Kamyab <87451309+kmybrostmi@users.noreply.github.com>
2022-10-10 20:05:46 +0330

    Add Role Aggregate

---
+++ Shop.Domain/RoleAgg/Role.cs
@@ -0,0 +1,48 @@
+﻿using Common.Domain;
+using Shop.Domain.OrderAgg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.RoleAgg;
+public class Role : AggregateRoot
+{
+    private Role()
+    {
+
+    }
+    public Role(string title, List<RolePermission> permissions)
+    {
+        NullOrEmptyDomainDataException.CheckString(title, nameof(title));
+        Title = title;
+        Permissions = permissions;
+    }
+    public Role(string title)
+    {
+        NullOrEmptyDomainDataException.CheckString(title, nameof(title));
+        Title = title;
+        Permissions = new List<RolePermission>();
+    }
+
+    public string Title { get; private set; }
+    public List<RolePermission> Permissions { get; private set; }
+
+    public void SetPermissions(List<RolePermission> permissions)
+    {
+        Permissions = permissions;
+    }
+
+    public void Edit(string title)
+    {
+        NullOrEmptyDomainDataException.CheckString(title, nameof(title));
+        Title = title;
+    }
+}
+
+
+
+
+
+
---
+++ Shop.Domain/RoleAgg/RolePermission.cs
@@ -0,0 +1,16 @@
+﻿using Common.Domain;
+using Shop.Domain.RoleAgg.Enums;
+
+namespace Shop.Domain.RoleAgg;
+
+public class RolePermission : BaseEntity
+{
+    public RolePermission(Permission permission)
+    {
+        Permission = permission;
+    }
+
+    public Guid RoleId { get; internal set; }
+    public Permission Permission { get; internal set; }
+}
+
