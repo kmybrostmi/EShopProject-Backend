04800967c71b7f9090c1d8917babdb1ed4e22bd1
Kamyab <87451309+kmybrostmi@users.noreply.github.com>
2022-10-11 20:30:31 +0330

    Complete Product Aggregate

--- Shop.Domain/ProductAgg/Product.cs
+++ Shop.Domain/ProductAgg/Product.cs
@@ -1,6 +1,8 @@
 ﻿using Common.Domain;
+using Common.Domain.Utils;
 using Common.Domain.ValueObjects;
 using Shop.Domain.OrderAgg;
+using Shop.Domain.ProductAgg.Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,15 +17,16 @@ public class Product : AggregateRoot
     }
 
     public Product(string title, string imageName, string description, Guid categoryId, Guid subCategoryId, 
-        Guid? secondarySubCategoryId, string slug, SeoData seoData)
+        Guid? secondarySubCategoryId, string slug, SeoData seoData, IProductDomainService domainService)
     {
+        Guard(title, slug, description, domainService);
         Title = title;
         ImageName = imageName;
         Description = description;
         CategoryId = categoryId;
         SubCategoryId = subCategoryId;
         SecondarySubCategoryId = secondarySubCategoryId;
-        Slug = slug;
+        Slug = slug.ToSlug();
         SeoData = seoData;
     }
     
@@ -40,14 +43,16 @@ public class Product : AggregateRoot
 
 
     public void Edit(string title, string description, Guid categoryId,
-            Guid subCategoryId, Guid? secondarySubCategoryId, string slug, SeoData seoData)
+            Guid subCategoryId, Guid? secondarySubCategoryId, string slug, SeoData seoData,
+            IProductDomainService domainService)
     {
+        Guard(title, slug, description, domainService);
         Title = title;
         Description = description;
         CategoryId = categoryId;
         SubCategoryId = subCategoryId;
         SecondarySubCategoryId = secondarySubCategoryId;
-        Slug = slug;
+        Slug = slug.ToSlug();
         SeoData = seoData;
     }
 
@@ -71,6 +76,18 @@ public class Product : AggregateRoot
     {
         Specifications.ForEach(s => s.ProductId = Id);
         Specifications = specifications;
+    }
+
+    public void Guard(string title, string slug, string description,
+            IProductDomainService domainService)
+    {
+        NullOrEmptyDomainDataException.CheckString(title, nameof(title));
+        NullOrEmptyDomainDataException.CheckString(description, nameof(description));
+        NullOrEmptyDomainDataException.CheckString(slug, nameof(slug));
+
+        if (slug != Slug)
+            if (domainService.IsSlugExist(slug.ToSlug()))
+                throw new SlugIsDuplicateException();
     }
 }
 
