0940619dfabb9784042e7c8ff13052239eb6a82c
Kamyab <87451309+kmybrostmi@users.noreply.github.com>
2022-10-27 22:35:46 +0330

    Add Relations

---
+++ Shop.Infrastructure/Migrations/20221027180441_Create_Relation.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop.Infrastructure.Migrations
+{
+    public partial class Create_Relation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                    ALTER TABLE [Seller].Inventories
+                    ADD CONSTRAINT FK_Inventories_Product_ProductId
+                    FOREIGN KEY (ProductId) REFERENCES [Product].Product(Id);
+                ");
+
+            migrationBuilder.Sql(@"
+                    ALTER TABLE [Order].Items
+                    ADD CONSTRAINT FK_Items_Inventories_InventoryId
+                    FOREIGN KEY (InventoryId) REFERENCES [Seller].Inventories(Id);
+                ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
