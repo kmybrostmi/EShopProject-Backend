2fa013fbbd348d870e78a6980619fb0d6db6e437
Kamyab <87451309+kmybrostmi@users.noreply.github.com>
2022-11-04 12:56:45 +0330

    Add User And UserAddress Controller

---
+++ Shop.Api/Controllers/UserAddressController.cs
@@ -0,0 +1,41 @@
+﻿using Common.AspNetCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Application.Aggregates.Users.AddAddress;
+using Shop.Application.Aggregates.Users.DeleteAddress;
+using Shop.Application.Aggregates.Users.EditAddress;
+using Shop.Presentation.Facade.Aggregates.Users.UserAddress;
+
+namespace Shop.Api.Controllers;
+public class UserAddressController : ApiController
+{
+	private readonly IUserAddressFacade _addressFacade;
+
+	public UserAddressController(IUserAddressFacade addressFacade)
+	{
+		_addressFacade = addressFacade;
+	}
+
+	[HttpPost]	
+	public async Task<ApiResult> AddUserAddress(AddUserAddressCommand command)
+	{
+		var result = await _addressFacade.AddUserAddress(command);
+		return CommandResult(result);
+	}
+
+	[HttpPut]
+	public async Task<ApiResult> EditUserAddress(EditUserAddressCommand command)
+	{
+		var result = await _addressFacade.EditUserAddress(command);
+		return CommandResult(result);
+	}
+
+	[HttpDelete]
+	public async Task<ApiResult> DeleteUserAddress(DeleteUserAddressCommand command)
+	{
+	    var result = await _addressFacade.DeleteUserAddress(command);
+		return CommandResult(result);
+	}
+}
+
+
---
+++ Shop.Api/Controllers/UserController.cs
@@ -0,0 +1,71 @@
+﻿using Common.AspNetCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Application.Aggregates.Users.ChargeWallet;
+using Shop.Application.Aggregates.Users.Create;
+using Shop.Application.Aggregates.Users.Edit;
+using Shop.Application.Aggregates.Users.Register;
+using Shop.Presentation.Facade.Aggregates.Users;
+using Shop.Query.Aggregates.Users.DTOs;
+
+namespace Shop.Api.Controllers;
+public class UserController : ApiController
+{
+	private readonly IUserFacade _userFacade;
+
+	public UserController(IUserFacade userFacade)
+	{
+		_userFacade = userFacade;
+	}
+
+	[HttpGet("{userId}")]
+	public async Task<ApiResult<UserDto?>> GetUserById(Guid userId)
+	{
+		var result = await _userFacade.GetUserById(userId);
+		return QueryResult(result);
+	}
+
+	[HttpGet("{phoneNumber}")]
+	public async Task<ApiResult<UserDto?>> GetUserByPhoneNumber(string phoneNumber)
+	{
+		var result = await _userFacade.GetUserByPhoneNumber(phoneNumber);
+		return QueryResult(result);
+	}
+
+	[HttpGet]
+	public async Task<ApiResult<UserFilterResult>> GetUserByFilter([FromQuery] UserFilterParams filterParams)
+	{
+		var result = await _userFacade.GetUserByFilter(filterParams);
+		return QueryResult(result);
+	}
+
+	[HttpPost]
+	public async Task<ApiResult> CreateUser(CreateUserCommand command)
+	{
+		var result = await _userFacade.CreateUser(command);
+		return CommandResult(result);
+	}
+
+	[HttpPost]
+	public async Task<ApiResult> RegisterUser(RegisterUserCommand command)
+	{
+		var result = await _userFacade.RegisterUser(command);
+		return CommandResult(result);
+	}
+
+	[HttpPut]
+	public async Task<ApiResult> EditUser(EditUserCommand command)
+	{
+		var result = await _userFacade.EditUser(command);
+		return CommandResult(result);
+	}
+
+	[HttpPut]
+	public async Task<ApiResult> ChargeWallet(ChargeUserWalletCommand command)
+	{
+		var result = await _userFacade.ChargeUserWallet(command);
+		return CommandResult(result);
+	}
+}
+
+
