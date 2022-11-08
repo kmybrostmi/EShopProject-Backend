d441729383fe5e30d2d52818c54c1804454c2035
Kamyab <87451309+kmybrostmi@users.noreply.github.com>
2022-11-08 18:32:24 +0330

    Fix Project

---
+++ Shop.Infrastructure/Migrations/20221107095839_addTokenTODb.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop.Infrastructure.Migrations
+{
+    public partial class addTokenTODb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
